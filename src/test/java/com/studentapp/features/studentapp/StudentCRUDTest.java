f1b8e7ed8873c345da614a910a89f0a9ca7819c9
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-11-07 09:23:23 +0100

    finish implement StudentClass, TestBase. Working on StudentCRUDT class

--- src/test/java/com/studentapp/features/studentapp/StudentCRUDTest.java
+++ src/test/java/com/studentapp/features/studentapp/StudentCRUDTest.java
@@ -1,7 +1,23 @@
 package com.studentapp.features.studentapp;
 
+import com.studentapp.testbase.TestBase;
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+
 /**
  * Created by magn on 11/6/2017.
  */
-public class StudentCRUDTest {
+public class StudentCRUDTest extends TestBase{
+
+    @Title("This test will create the student")
+    @Test
+    public void createStudent() {
+        SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body("")
+                .post();
+    }
 }
