e67b3d4922f6f9471f936a0564263c134b890112
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-11-07 09:23:23 +0100

    finish implement StudentClass, TestBase. Working on StudentCRUDT class

--- src/test/java/com/studentapp/testbase/TestBase.java
+++ src/test/java/com/studentapp/testbase/TestBase.java
@@ -1,7 +1,15 @@
 package com.studentapp.testbase;
 
+import com.gargoylesoftware.htmlunit.javascript.host.ReadableStream;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
 /**
  * Created by magn on 11/6/2017.
  */
 public class TestBase {
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://localhost:8080/student/list";
+    }
 }
