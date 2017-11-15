398744bb94076fecc8c5e4861cb1e48951993d38
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-11-15 10:19:01 +0100

    set up pom.xml file in order to taging and running test

---
+++ src/test/java/com/studentapp/features/studentapptest/TagsTestTutorial.java
@@ -0,0 +1,53 @@
+package com.studentapp.features.studentapptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.studentapp.testbase.TestBase;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Title;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+
+
+@RunWith(SerenityRunner.class)
+public class TagsTestTutorial extends TestBase {
+
+    @WithTag("Studentfeature:NEGATIVE")
+    @Title("Provide a 405 status code when incorrect HTTP method is used to access a resource")
+    @Test
+    public void inValidMethod() {
+        SerenityRest.
+                rest().given().when()
+                .post("/list")
+                .then()
+                .statusCode(405)
+                .log().all();
+    }
+
+
+    @WithTag("Studentfeature:POSITIVE")
+    @Test
+    @Title("This test will verify if a status code of 200 is returned for GET request")
+    public void verifyIftheStatusCodeIs200() {
+        SerenityRest.
+                rest().given().when()
+                .get("/list")
+                .then()
+                .statusCode(200);
+    }
+
+    @WithTag("Studentfeature:NEGATIVE")
+    @Title("This test will provide an error code of 400 when user tries to access an invalid resource")
+    @Test
+    public void incorrectResource() {
+        SerenityRest.
+                rest().given().when()
+                .get("/listsfkh")
+                .then()
+                .statusCode(400)
+                .log().all();
+    }
+}
